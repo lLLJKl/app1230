15b55b783cb529eb8b1a55e45183e2b39d96011c
Dean <hehe627@gmail.com>
2025-12-30 17:52:13 +0900

    router

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // base: "/", 
+  // server: {
+  //   host: true,
+  // }, 
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url))
+    }
+  }
+})
