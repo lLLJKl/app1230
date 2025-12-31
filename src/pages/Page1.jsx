07610405ca9a0ea0a1f54017de146fe7f317c74a
mpsddd-commits <mpsddd@gmail.com>
2025-12-31 11:30:54 +0900

    잠시 저장

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,3 +1,49 @@
-const Page1 = () => <h1>LEE HYUN SEO</h1>
+const Page1 = () => {
+
+    const onclick = () => {
+        //location"location.href = 'Select.html';"
+    } 
+
+    const arr = [
+        { "name":"스티븐" , "email":"jobs@shellfolder.com", "regDate":"2023-02-28"},
+        { "name":"에브릴" , "email":"lavigne@shellfolder.com", "regDate":"2023-02-27"},
+    ]
+
+    const styles ={
+        "cursor": "pointer"
+    }
+
+return (
+    <div className="container mt-3">
+	  <h1 className="display-1 text-center">사용자 목록</h1>
+	  <div className="btn-group">
+	    <a href="Create.html" className="btn btn-primary">사용자 추가</a>
+	  </div>
+	  <table className="table table-hover mt-3">
+	    <thead className="table-dark">
+	      <tr>
+	      	<th>이름</th>
+	        <th>이메일</th>
+	        <th>가입날짜</th>
+	      </tr>
+	    </thead>
+	    <tbody>
+	      {
+            arr.map((v,i) =>{
+                return(
+                    <tr style={styles} onClick={onclick} key={i}>
+	                    <td>{ v.name}</td>
+	                    <td>{ v.email}</td>
+	                    <td>{v.regDate}</td>
+	                </tr>
+                )
+            })
+          }
+	      
+	    </tbody>
+	  </table>
+	</div>
+
+)}
 
 export default Page1
