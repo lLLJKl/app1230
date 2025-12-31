ee0fdff8deca46eda383ea0fb1ee999d011c5cb2
mpsddd-commits <mpsddd@gmail.com>
2025-12-31 16:50:34 +0900

    리액트 use이펙,스테이트,로케이션,네비포함

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,46 +1,23 @@
 // import { createBrowserRouter } from "react-router";
 // import { RouterProvider } from "react-router/dom";
-import {BrowserRouter, Routes, Route} from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Page1 from '@pages/Page1.jsx'
 import Page2 from '@pages/Page2.jsx'
-import Page3 from '@pages/Page3.jsx'
-import Page4 from '@pages/Page4.jsx'
 
 const Router = () => {
   // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     Component: Page1
-  //   },
-  //   {
-  //     path: "/page2",
-  //     Component: Page2
-  //   },
-  //   {
-  //     path: "/page3",
-  //     Component: Page3
-  //   },
-  //   {
-  //     path: "/page4",
-  //     Component: Page4
-  //   },
-
+    // { path: "/", Component: Page1 },
+    // { path: "/page2", Component: Page2 },
   // ]);
   // return <RouterProvider router={router} />
-  return(
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Page1/>} />
-        <Route path="/page2" element={<Page2/>} />
-        <Route path="/page3" element={<Page3/>} />
-        <Route path="/page4" element={<Page4/>} />
+        <Route path="/" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
       </Routes>
-
     </BrowserRouter>
   )
-
-
-
 }
 
 export default Router
